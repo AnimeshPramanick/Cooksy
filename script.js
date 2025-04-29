66e131582a7734b45205f22a45cacca49a41e8cc
Animesh Pramanick <apramanick916@gmail.com>
2025-04-29 18:40:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,35 @@ searchBtn.addEventListener("click", (e) => {
     fetchRecipe(searchInput);
     console.log("Hello");
 });
+
+
+
+const fetchRandomRecipes = async () => {
+  topText.innerHTML = "<h1>Suggestion</h1>";
+  recipeContainer.innerHTML = "";
+
+  for (let i = 0; i < 21; i++) {
+    const res = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const data = await res.json();
+    const meal = data.meals[0];
+
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+    recipeDiv.innerHTML = `<img src="${meal.strMealThumb}">
+        <h2>${meal.strMeal}</h2>
+        <p>${meal.strArea} Dish</p>
+        <p>${meal.strCategory}</p>`;
+
+    const view = document.createElement("button");
+    view.textContent = "VIEW RECIPE";
+    view.addEventListener("click", () => {
+      window.location.href = `recipe.html?id=${meal.idMeal}`;
+    });
+
+    recipeDiv.appendChild(view);
+    recipeContainer.appendChild(recipeDiv);
+  }
+};
+window.addEventListener("DOMContentLoaded", fetchRandomRecipes);
